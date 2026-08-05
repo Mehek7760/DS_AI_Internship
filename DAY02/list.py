e6d161eb6f87f838baf317dc7c7f437d0243d498
Mehek7760 <mehekfmehek@gmail.com>
2026-08-05 11:42:15 +0530

    5 commit

--- DAY02/list.py
+++ DAY02/list.py
@@ -1,8 +1,2 @@
-a = ["akash", 25, 23,23, 24]
-
-print("Name :", a[0])
-print("Age :", a[1])
-print("Marks :")
-print(a[1])
-print(a[2])
-print(a[3])
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
