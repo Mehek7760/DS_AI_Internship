a452e813c0e75350c2911cbed17cf164e0b0e69d
Mehek7760 <mehekfmehek@gmail.com>
2026-08-05 10:41:10 +0530

    4 commit

--- DAY01/PROGRAM1.py
+++ DAY01/PROGRAM1.py
@@ -1,14 +1,6 @@
 print("Hello World")
 
 
-while temp > 0:
-    digit = temp % 10
-    rev = rev * 10 + digit
-    temp = temp // 10
 
-if num == rev:
-    print("Palindrome")
-else:
-    print("Not a Palindrome")
 
     
