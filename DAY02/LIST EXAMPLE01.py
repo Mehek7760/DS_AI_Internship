70bb44bf5cad069f421e7d927104943271119921
Mehek7760 <mehekfmehek@gmail.com>
2026-08-05 12:08:00 +0530

    6 commit

---
+++ DAY02/LIST EXAMPLE01.py
@@ -0,0 +1,14 @@
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
+
+mylist = ['apple', 'banana', 'cherry']
+print(mylist[-1])
+
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+
