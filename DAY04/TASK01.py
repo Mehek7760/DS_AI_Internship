a940000022417b65354ba75047e4c96c25f416bb
Mehek7760 <mehekfmehek@gmail.com>
2026-08-07 12:25:36 +0530

    12 commit

---
+++ DAY04/TASK01.py
@@ -0,0 +1,9 @@
+import math
+
+num = float(input("Enter a number: "))
+
+print("Square Root =", math.sqrt(num))
+print("Square =", math.pow(num, 2))
+print("Ceiling Value =", math.ceil(num))
+print("Floor Value =", math.floor(num))
+print("Value of Pi =", math.pi)
