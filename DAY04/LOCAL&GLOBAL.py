88295e7eb2a1204929fa5b59e7023bcbf308ba37
Mehek7760 <mehekfmehek@gmail.com>
2026-08-07 12:12:26 +0530

    11 commit

---
+++ DAY04/LOCAL&GLOBAL.py
@@ -0,0 +1,16 @@
+x=200
+def myfunc():
+  x = 300
+  print(x)
+myfunc()
+print(x)
+
+shop_name = "More"
+
+def shopping():
+    item = "iPhone"
+    price = 90000
+    print(shop_name)
+    print(item, price)
+
+shopping()
